6e8b313be21ca92115b3c78ec86673ee17169d58
PaulRichez <paul.richez59@gmail.com>
2022-11-21 06:55:44 +0100

    fix

--- server/src/plugins/emailengine/server/utils/IMAPSMTP.js
+++ server/src/plugins/emailengine/server/utils/IMAPSMTP.js
@@ -1,5 +1,5 @@
 module.exports = {
-    getIMAPSMTP: async function  getEmailengineUrl(strapi,strapi) {
+    getIMAPSMTP: async function  getIMAPSMTP(strapi) {
         return await strapi.entityService.findMany(
             'api::imap-smtp.imap-smtp',
             { populate: ['imap', 'smtp'] }
